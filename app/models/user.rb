8ff5152c04c76fa9761cf272916e1be545af632e
Jesse Kopala <jdkopala@gmail.com>
2022-08-01 15:40:36 -0600

    Implement rspec testing for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
   has_secure_password
-  
+
+  validates :password, presence: true
+  validates_length_of :password, minimum: 6
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
