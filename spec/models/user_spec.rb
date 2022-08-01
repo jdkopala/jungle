4264dd776e3e1176893cd121d4f799c143cc645f
Jesse Kopala <jdkopala@gmail.com>
2022-08-01 16:34:46 -0600

    Implement edge case tests for .authenticate_with_credentials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
       @user = User.create(first_name: "Jesse", last_name: "Kopala", email: nil, password: "test", password_confirmation: "test")
       expect(@user).to be_invalid
     end
-    it "Should not create a new user if the email already exists in the database" do
+    it "Should not create a new user if the email already exists in the database, regardless of case" do
       @user1 = User.create(first_name: "Jesse", last_name: "Kopala", email: "TEST@TEST.COM", password: "tester2", password_confirmation: "tester2")
       expect(@user1).to be_valid
       @user2 = User.create(first_name: "Jim", last_name: "Bob", email: "test@test.com", password: "tester2", password_confirmation: "tester2")
@@ -50,6 +50,20 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
       auth = User.authenticate_with_credentials(@user.email, "test")
       expect(auth).to eq(nil)
+    end
+    it "Should authenticate the user even if there are extra spaces in the email field" do
+      @user = User.create(first_name: "Jesse", last_name: "Kopala", email: "test2@test.com", password: "tester2", password_confirmation: "tester2")
+      expect(@user).to be_valid
+      auth = User.authenticate_with_credentials("  test2@test.com ", "tester2")
+      expect(auth).to eq(@user)
+    end
+    it "Should authenticate the user even if the email case does not match" do
+      @user = User.create(first_name: "Jesse", last_name: "Kopala", email: "test2@test.com", password: "tester2", password_confirmation: "tester2")
+      expect(@user).to be_valid
+      auth = User.authenticate_with_credentials("TEST2@test.com", "tester2")
+      expect(auth).to eq(@user)
+      auth2 = User.authenticate_with_credentials("tEst2@teSt.COM", "tester2")
+      expect(auth2).to eq(@user)
     end
   end
 
